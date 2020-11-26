0158801877e0c8bc0f9f740c2a2dc3d21a58da00
Britton Upchurch <tobritton@gmail.com>
2020-11-26 20:19:07 +1100

    Killing gunicorn parent, unbatched sentence generation

--- perciapp/blueprints/create/views.py
+++ perciapp/blueprints/create/views.py
@@ -72,33 +72,24 @@ def create_description():
         create = Create(**params)
         create.save_and_update_user(current_user)
 
-        first1 = 'sent1/sent1_2/sent1_3'
-        first2 = 'sent1_4/sent1_5/sent1_6'
-        first3 = 'sent1_7/sent1_8/sent1_9'
-        first4 = 'sent1_16/sent1_17/sent1_18'
-        first5 = 'sent1_10/sent1_11/sent1_12'
-        first6 = 'sent1_13/sent1_14'
-        first7 = 'sent1_15/sent1_19'
-        firsts = [first1, first2, first3, first4, first5, first6, first7]
+        firsts = ['sent1','sent1_2', 'sent1_3', 'sent1_4',
+                  'sent1_5', 'sent1_6', 'sent1_7', 'sent1_8',
+                  'sent1_9','sent1_16', 'sent1_17', 'sent1_18',
+                  'sent1_10', 'sent1_11', 'sent1_12','sent1_13', 'sent1_14',
+                  'sent1_15', 'sent1_19']
 
-        second1 = 'sent2/sent2_2/sent2_3'
-        second2 = 'sent2_4/sent2_5/sent2_6'
-        second3 = 'sent2_7/sent2_8/sent2_9'
-        second4 = 'sent2_16/sent2_17/sent2_18'
-        second5 = 'sent2_10/sent2_11/sent2_12'
-        second6 = 'sent2_13/sent2_14'
-        second7 = 'sent2_15/sent2_19'
-        seconds = [second1, second2, second3, second4, second5, second6, second7]
+        seconds = ['sent2','sent2_2', 'sent2_3', 'sent2_4',
+                  'sent2_5', 'sent2_6', 'sent2_7', 'sent2_8',
+                  'sent2_9','sent2_16', 'sent2_17', 'sent2_18',
+                  'sent2_10', 'sent2_11', 'sent2_12','sent2_13', 'sent2_14',
+                  'sent2_15', 'sent2_19']
 
 
-        third1 = 'sent3/sent3_2/sent3_3'
-        third2 = 'sent3_4/sent3_5/sent3_6'
-        third3 = 'sent3_7/sent3_8/sent3_9'
-        third4 = 'sent3_10/sent3_11/sent3_12'
-        third5 = 'sent3_13/sent3_14/sent3_15'
-        third6 = 'sent3_16/sent3_17'
-        third7 = 'sent3_18/sent3_19'
-        thirds = [third1, third2, third3, third4, third5, third6, third7]
+        thirds = ['sent3','sent3_2', 'sent3_3', 'sent3_4',
+                  'sent3_5', 'sent3_6', 'sent3_7', 'sent3_8',
+                  'sent3_9','sent3_16', 'sent3_17', 'sent3_18',
+                  'sent3_10', 'sent3_11', 'sent3_12','sent3_13', 'sent3_14',
+                  'sent3_15', 'sent3_19']
 
         project_id = "perciapp"
         topic_id = "description-order"
@@ -109,7 +100,7 @@ def create_description():
         for i in firsts:
             data = str(create.id)
             data = data.encode("utf-8")
-            future = publisher.publish(topic_path, data, labels=i, id=str(create.id))
+            future = publisher.publish(topic_path, data, label=i, id=str(create.id))
             print(future.result())
 
         topic_id = "description-order-sent-2"
@@ -119,7 +110,7 @@ def create_description():
         for i in seconds:
             data = str(create.id)
             data = data.encode("utf-8")
-            future = publisher.publish(topic_path, data, labels=i, id=str(create.id))
+            future = publisher.publish(topic_path, data, label=i, id=str(create.id))
             print(future.result())
         
         topic_id = "description-order-sent-3"
@@ -129,7 +120,7 @@ def create_description():
         for i in thirds:
             data = str(create.id)
             data = data.encode("utf-8")
-            future = publisher.publish(topic_path, data, labels=i, id=str(create.id))
+            future = publisher.publish(topic_path, data, label=i, id=str(create.id))
             print(future.result())
 
         print()
@@ -188,10 +179,10 @@ def routesent1():
     from perciapp.blueprints.create.tasks import generate_sent1
     message = request.get_json()['message']
     description_id = int(base64.b64decode(message['data']).decode('utf-8').strip())
-    labels = message['attributes']['labels']
-    generate_sent1(description_id,labels)
+    label = message['attributes']['label']
+    generate_sent1(description_id,label)
     # res = requests.get('https://perciappprocessor-4v6rgmnwrq-uc.a.run.app/create/shutdown')
-    os.kill(os.getpid(), signal.SIGTERM)
+    os.kill(self._parent_pid, signal.TERM)
     return ('', 204)
 
 @create.route('/gensent2/', methods=['POST'])
@@ -200,10 +191,10 @@ def routesent2():
     from perciapp.blueprints.create.tasks import generate_sent2
     message = request.get_json()['message']
     description_id = int(base64.b64decode(message['data']).decode('utf-8').strip())
-    labels = message['attributes']['labels']
-    generate_sent2(description_id,labels)
+    label = message['attributes']['label']
+    generate_sent2(description_id,label)
     # res = requests.get('https://perciappprocessor-4v6rgmnwrq-uc.a.run.app/create/shutdown')
-    os.kill(os.getpid(), signal.SIGTERM)
+    os.kill(self._parent_pid, signal.TERM)
     return ('', 204)
 
 @create.route('/gensent3/', methods=['POST'])
@@ -212,10 +203,10 @@ def routesent3():
     from perciapp.blueprints.create.tasks import generate_sent3
     message = request.get_json()['message']
     description_id = int(base64.b64decode(message['data']).decode('utf-8').strip())
-    labels = message['attributes']['labels']
-    generate_sent3(description_id,labels)
+    label = message['attributes']['label']
+    generate_sent3(description_id,label)
     # res = requests.get('https://perciappprocessor-4v6rgmnwrq-uc.a.run.app/create/shutdown')
-    os.kill(os.getpid(), signal.SIGTERM)
+    os.kill(self._parent_pid, signal.TERM)
     return ('', 204)
 
 @create.route('/editsent1/', methods=['POST'])
